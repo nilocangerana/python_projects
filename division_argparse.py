844feed940eb6ed97e0788cd751662cffeabe2bf
Nilo Cangerana <40279655+nilocangerana@users.noreply.github.com>
2023-11-18 17:16:41 -0300

    Update division_argparse.py

--- division_argparse.py
+++ division_argparse.py
@@ -3,18 +3,18 @@ import argparse
 mgs = "The Division ArgParser."
 parser = argparse.ArgumentParser(description=mgs)
 
-parser.add_argument('value_v1', type=float, nargs='?', help='a value to be divided', default=0)
-parser.add_argument('value_v2', type=float, nargs='?', help='a value to be the divisor', default=1)
+parser.add_argument('values', type=float, nargs=2, help='values to be divided', default=1)
 
-parser.add_argument('-d','--Division',
-                    help='divide the values: v1/v2')
+parser.add_argument('-d','--Division', action='store_true', help='divide the values: v1/v2')
 
 args = parser.parse_args()
 
-def division():
-  return str(args.value_v1/args.value_v2)
+print(args.values)
+
+def division(v1, v2):
+  return str(v1/v2)
 
 if args.Division:
-  print("The division: "+division())
+  print("The division: "+division(args.values[0], args.values[1]))
 else:
-  print("Comand not received")
+  print("Command not received")
