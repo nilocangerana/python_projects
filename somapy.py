5869f77e3eacaf7e3f3948d98464e7833fe2615d
Nilo Cangerana <40279655+nilocangerana@users.noreply.github.com>
2023-11-18 15:22:27 -0300

    Soma args

---
+++ somapy.py
@@ -0,0 +1,31 @@
+import getopt, sys
+
+argumentList = sys.argv[1:]
+
+options = "hmo:"
+long_options = ["Help", "My_file", "Output="]
+
+sum_values=[]
+
+try:
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    for currentArgument, currentValue in arguments:
+ 
+        if currentArgument in ("-h", "--Help"):
+            print ("Displaying Help")
+        elif currentArgument in ("-m", "--My_file"):
+            print ("Displaying file_name:", sys.argv[0])
+        elif currentArgument in ("-o", "--Output"):
+            sum_values.append(int(currentValue))
+            print (("value (% s)") % (currentValue))
+             
+except getopt.error as err:
+    print (str(err))
+    
+def Somar():
+    total=0
+    for i in sum_values:
+        total=total+sum_values[i]
+    return str(total)
+    
+print("soma: "+Somar())
